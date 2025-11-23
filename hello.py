713060ddaf17c5e3985f6972408f56bdea360c2b
svepodd <svepodd.bmstu@gmail.com>
2025-11-23 22:28:41 +0300

    New comment in hello.py

--- hello.py
+++ hello.py
@@ -5,6 +5,7 @@ def main(
     lastname: str = typer.Option("", help="Фамилия пользователя."),
     formal: bool = typer.Option(False, "--formal", "-f", help="Использовать формальное приветствие."),
 ):
+    # New comment from SvePodd
     """
     Говорит "Привет" пользователю, опционально используя фамилию и формальный стиль.
     """
